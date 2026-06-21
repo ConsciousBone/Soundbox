1bcc82bcdeb4d3daf239ae428e3c0ce8f9e2d16a
Evan <68429595+ConsciousBone@users.noreply.github.com>
2026-06-21 15:46:05 +0100

    Initial Commit

---
+++ Soundbox/SoundboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundboxApp.swift
+//  Soundbox
+//
+//  Created by Evan Plant on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SoundboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
